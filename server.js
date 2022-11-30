b827717bfa7504f352c9557867e1f5f43109ba18
manish <manishkumar60708090@gmail.com>
2022-12-01 00:58:03 +0530

    explained with backend

--- server.js
+++ server.js
@@ -1,18 +1,26 @@
 
 const express = require('express')
-
+const cors = require('cors');
 const app = express()
+
+app.use(cors());
+
 
 app.get('/' , (req , res) => {
     res.send('welcome from home ')
 })
 
+
 app.get("/room" , (req , res) => {
     res.send('welcome from rooms')
 })
 
+
 app.listen(8000 , () => {
     console.log('server is listen......')
 })
+
+
+
 
 
